d271432464a61a0242c3fd947e0440d11f8d5ee8
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-24 20:32:31 +0000

    Process-argv

--- NODE PROCESS-ARGV/index.js
+++ NODE PROCESS-ARGV/index.js
@@ -2,4 +2,10 @@
 console.log(process.argv);
 
 // arguments passed from the command line are accessed by index
-// console.log(process.argv[2]);
+console.log(process.argv[2]);
+console.log(process.argv[10]);
+
+
+const a=process.argv[3];
+const b=process.argv[5];
+console.log(a+b);
