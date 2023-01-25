60a11567833db11c5bd3aa01df85960e147b8ef4
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-25 13:50:19 +0000

    Modularization - export, import by require, methods

--- NODE MODULARIZATION/badmath.js
+++ NODE MODULARIZATION/badmath.js
@@ -2,8 +2,27 @@ const pie = 'apple';
 
 const predictable = () => 1;
 
+
+
+
+const add=(a,b)=>{
+  return a+b;
+}
+
+const sub= (a,b)=>{
+  return a-b;
+}
+
+const power= (a,b)=>{
+  return Math.pow(a,b);
+}
+
+
 // module.exports is an object we use to store variables or methods
 module.exports = {
   pie,
   predictable,
+  add,
+  sub,
+  power
 };
--- NODE MODULARIZATION/index.js
+++ NODE MODULARIZATION/index.js
@@ -3,3 +3,9 @@ const badmath = require('./badmath.js');
 console.log(badmath.pie);
 
 console.log(badmath.predictable());
+
+console.log(badmath.add(2,3));
+
+console.log(badmath.sub(2,3));
+
+console.log(badmath.power(2,3));
