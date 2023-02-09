1e1d65781a3ff88778ea4436615ba92ed541965c
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-09 15:02:34 +0000

    Test Driven development - Example :reverse,palindrome,capitalize

--- 02-Stu-TDD/starter/test/algo.test.js
+++ 02-Stu-TDD/starter/test/algo.test.js
@@ -2,14 +2,42 @@ const Algo = require("../algo");
 
 describe("Algo", () => {
   describe("reverse", () => {
+    it("shouls reverse a string", ()=> {
+      const str="Hello";
+      const rev="olleH";
 
+      const res=new Algo().reverse(str);
+      expect(res).toEqual(rev);
+    });
   });
+
+
+
 
   describe("isPalindrome", () => {
-   
+    it("Return true if Palindrome",()=>{
+        const str="raceacar";
+        const res=new Algo().isPalindrome(str);
+        expect(res).toEqual(true);
+    }); 
+
+    it("Return false , if not Palindrome",()=>{
+       const str="neon";
+       const res=new Algo().isPalindrome(str);
+       expect(res).toEqual(false);
+    });
   });
 
+
+
+
+
   describe("capitalize", () => {
-    
+    it("Return by capitalizing the first letter of each word capitalized",()=>{
+      const str="capitalize each word of the string";
+      const cap="Capitalize Each Word Of The String";
+      const res=new Algo().capitalize(str);
+      expect(res).toEqual(cap);
+    });
   });
 });
